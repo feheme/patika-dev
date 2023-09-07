2f04d190afe24faf51ffb43917f0976e5fbbd9a6
Fatih Hakan Mehmet CAYLIDEMIRCI <cyldmrcfhm@gmail.com>
2023-09-07 10:38:13 +0300

    changes

--- .net-core/variables-and-datatypes/Program.cs
+++ .net-core/variables-and-datatypes/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+string str1 = string.Empty;
+
+str1 = "Zikriye Ürkmez";
+
+string ad = "Zikriye";
+string soyad = "Ürkmez";
+string tamIsism = ad + " " + soyad;
+
+Console.WriteLine(tamIsism);
+
