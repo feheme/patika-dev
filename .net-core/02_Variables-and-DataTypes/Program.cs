e4a49ac56bbea8cadb2fcb7cff45181aef21e628
Fatih Hakan Mehmet CAYLIDEMIRCI <cyldmrcfhm@gmail.com>
2023-09-08 10:25:34 +0300

    modified variables

--- .net-core/02_Variables-and-DataTypes/Program.cs
+++ .net-core/02_Variables-and-DataTypes/Program.cs
@@ -1,28 +1,28 @@
-﻿byte b = 5;     //1byte
-sbyte c = 5;    //1byte
+﻿// byte b = 5;     //1byte
+// sbyte c = 5;    //1byte
 
-short s = 5;    //2byte
-ushort us = 5;   //2byte
+// short s = 5;    //2byte
+// ushort us = 5;   //2byte
 
-Int16 i16 = 2;  //2byte
-int i = 2;      //4byte
-Int32 i32 = 2;  //4byte
-Int64 i64 = 2;  //8byte
+// Int16 i16 = 2;  //2byte
+// int i = 2;      //4byte
+// Int32 i32 = 2;  //4byte
+// Int64 i64 = 2;  //8byte
 
-uint ui = 2;    //4byte
-long l = 4;     //8byte
-ulong ul = 4;   //8byte
+// uint ui = 2;    //4byte
+// long l = 4;     //8byte
+// ulong ul = 4;   //8byte
 
 //Real sayılar
-float f = 5;    //4byte
-double d = 5;   //8byte
-decimal de = 5; //16byte
+// float f = 5;    //4byte
+// double d = 5;   //8byte
+// decimal de = 5; //16byte
 
-char ch = '5';  //2byte
-string sr = "Nevzat";  //Sınırsız
+// char ch = '5';  //2byte
+// string sr = "Nevzat";  //Sınırsız
 
-bool b1 = true;
-bool b2 = false;
+// bool b1 = true;
+// bool b2 = false;
 
 DateTime dt = DateTime.Now;
 
@@ -36,9 +36,9 @@ object o4 = 4.3;
 //string ifadeler
 
 string str1 = string.Empty;
-str1 = "Nevzat Cirpicioglu";
-string ad = "Nevzat";
-string soyad = "Cirpicioglu";
+str1 = "feheme";
+string ad = "fatih";
+string soyad = "hakan";
 string tamIsim = ad + " " + soyad;
 
 //Integer
@@ -49,7 +49,7 @@ int integer3 = integer1 + integer2;
 
 //boolean
 
-bool bool1 = 10 > 2;
+// bool bool1 = 10 > 2;
 
 //Değişken Dönüşümleri
 string str20 = "20";
